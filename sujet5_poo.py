76471b836b4647be9ff3f864f855eb805f366a00
Typh <40773254+Typhaine64@users.noreply.github.com>
2024-09-23 17:17:06 +0200

    Add files via upload

---
+++ sujet5_poo.py
@@ -0,0 +1,42 @@
+#Sert a l'héritage pour définir un fils droit/gauche
+class Node:
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+#la constante défini un nombre qui n'a pas besoin d'être calculé
+
+class Constante(Node):
+    def __init__(self, value):
+        super().__init__()
+        self.data = value
+
+    def resolve(self):
+        return self.data
+
+# on creer des classes pour chacunes des opérations
+
+class Plus(Node):
+    def resolve(self):
+        return self.left.resolve() + self.right.resolve()
+
+class Moins(Node):
+    def resolve(self):
+        return self.left.resolve() - self.right.resolve()
+
+class Fois(Node):
+    def resolve(self):
+        return self.left.resolve() * self.right.resolve()
+
+class Divise(Node):
+    def resolve(self):
+        return self.left.resolve() / self.right.resolve()
+
+root = Moins(Plus(Constante(3), Fois(Constante(2), Constante(6))), Fois(Constante(2), Plus(Constante(4), Constante(9))))
+
+result = root.resolve()
+print(f"Result: {result}")
+
+racine = Fois(Constante(2), Moins(Plus(Constante(4), Fois(Constante(5),Constante(3))), Constante(16)))
+resultat = racine.resolve()
+print(f"Result: {resultat}")
